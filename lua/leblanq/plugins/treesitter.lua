5061673533294ac70677b50a6a64c0e18d10721d
Paul Le <paul@investorhub.com>
2024-12-25 02:21:27 +1100

    fix treesitter and add tabpage remaps

--- lua/leblanq/plugins/treesitter.lua
+++ lua/leblanq/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "markdown", "javascript", "typescript", "ruby", "elixir" },
+            ensure_installed = { "c", "cpp", "lua", "vim", "javascript", "typescript", "ruby", "elixir", "rust", "python", "json", "yaml", "html", "css", "scss", "graphql", "svelte", "haskell", "java", "php", "bash", "dockerfile", "go", "yaml" },
             sync_install = false,
             auto_install = true,
             highlight = { enable = true },
