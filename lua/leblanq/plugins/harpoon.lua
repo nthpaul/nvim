2c442af5c7dcc8997272ef9bf4e034c0a03a4766
Paul Le <paul@investorhub.com>
2024-12-26 15:07:15 +1100

    feat: more harpoon keymaps

--- lua/leblanq/plugins/harpoon.lua
+++ lua/leblanq/plugins/harpoon.lua
@@ -9,12 +9,20 @@ return {
 
         vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<C-m>", function() harpoon:list():select(5) end)
+
+        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
+
+        vim.keymap.set("n", "<leader><C-1>", function() harpoon:list():replace_at(1) end)
+        vim.keymap.set("n", "<leader><C-2>", function() harpoon:list():replace_at(2) end)
+        vim.keymap.set("n", "<leader><C-3>", function() harpoon:list():replace_at(3) end)
+        vim.keymap.set("n", "<leader><C-4>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader><C-5>", function() harpoon:list():replace_at(5) end)
     end
 }
