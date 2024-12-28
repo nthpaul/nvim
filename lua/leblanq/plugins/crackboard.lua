7c78bbc60fa018dd21579fa3f1e4a71ba4715cfe
Paul Le <paul@investorhub.com>
2024-12-28 22:03:23 +1100

    chore: fix crackboard

--- lua/leblanq/plugins/crackboard.lua
+++ lua/leblanq/plugins/crackboard.lua
@@ -1,13 +1,9 @@
 return {
-    require('lazy').setup({
-        {
-            'boganworld/crackboard.nvim',
-            dependencies = { 'nvim-lua/plenary.nvim' },
-            config = function()
-                require('crackboard').setup({
-                    session_key = '89b25402e5f9293d32b846df392efd3435cb8bbd606a81497ecb74e91bea3e29',
-                })
-            end,
-        }
-    })
+    'boganworld/crackboard.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require('crackboard').setup({
+            session_key = '89b25402e5f9293d32b846df392efd3435cb8bbd606a81497ecb74e91bea3e29',
+        })
+    end,
 }
