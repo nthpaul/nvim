83a8777242b43bcda23883ad176dcb766a7845ad
Paul Le <paul@investorhub.com>
2024-12-28 21:55:14 +1100

    feat: idc if the session key for crackboard is public

--- lua/leblanq/plugins/crackboard.lua
+++ lua/leblanq/plugins/crackboard.lua
@@ -5,7 +5,7 @@ return {
             dependencies = { 'nvim-lua/plenary.nvim' },
             config = function()
                 require('crackboard').setup({
-                    session_key = 'xx',
+                    session_key = '89b25402e5f9293d32b846df392efd3435cb8bbd606a81497ecb74e91bea3e29',
                 })
             end,
         }
