7879c9636823591ac3cafb6ff7669090c178973e
Paul Le <paul@investorhub.com>
2024-12-30 18:08:50 +1100

    chore: fix notifications bg

--- lua/leblanq/plugins/notify.lua
+++ lua/leblanq/plugins/notify.lua
@@ -1,6 +1,9 @@
 return {
     "rcarriga/nvim-notify",
     config = function()
-        vim.notify = require "notify"
+        vim.notify = require("notify")
+        vim.notify.setup({
+            background_colour = "#000000",
+        })
     end,
 }
