d924495619d364d32a2fc25990202e9818137479
Paul Le <paul@investorhub.com>
2025-01-07 14:53:53 +1100

    feat: remove filetree and make case insensitive search the default

--- lua/leblanq/plugins/filetree.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-        local ft = require("nvim-tree")
-        ft.setup()
-
-        vim.api.nvim_set_keymap("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-    end
-}
--- lua/leblanq/set.lua
+++ lua/leblanq/set.lua
@@ -13,6 +13,8 @@ vim.opt.undofile = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
+vim.opt.ignorecase = true
+
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
