95b5bf5ecdd9fd7d322717a0e79349a5bc944c4d
Paul Le <taulnle@gmail.com>
2025-02-09 15:02:14 +1100

    feat: save

--- lua/leblanq/init.lua
+++ lua/leblanq/init.lua
@@ -3,63 +3,63 @@ require("leblanq.remap")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "leblanq.plugins",
-    change_detection = { notify = false }
+  spec = "leblanq.plugins",
+  change_detection = { notify = false }
 })
 
 vim.o.background = "dark" -- or "light" for light mode
 -- vim.cmd([[colorscheme gruvbox]])
 -- vim.cmd([[colorscheme kanagawa]])
 -- vim.cmd([[colorscheme dayfox]])
--- vim.cmd([[colorscheme nightfox]])
-vim.cmd([[colorscheme terafox]])
+vim.cmd([[colorscheme nightfox]])
+-- vim.cmd([[colorscheme terafox]])
 
 local augroup = vim.api.nvim_create_augroup
 local leblanqGroup = augroup('leblanq', {})
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd('LspAttach', {
-    group = leblanqGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        -- commonly used
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>prn", function() vim.lsp.buf.rename() end, opts)
+  group = leblanqGroup,
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    -- commonly used
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>prn", function() vim.lsp.buf.rename() end, opts)
 
-        -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
-        -- error diagnostics in current buffer
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>fe", function()
-            vim.diagnostic.setloclist() -- or use setqflist({bufnr = 0}) if you prefer quickfix list
-        end, { desc = "List buffer diagnostics" })
+    -- error diagnostics in current buffer
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>fe", function()
+      vim.diagnostic.setloclist() -- or use setqflist({bufnr = 0}) if you prefer quickfix list
+    end, { desc = "List buffer diagnostics" })
 
-        vim.keymap.set("n", "<leader>pe", function()
-            -- Get all diagnostics from the workspace
-            vim.diagnostic.setqflist({
-                severity = nil,  -- Get all severities
-                workspace = true -- Get diagnostics from all files in workspace
-            })
-        end, { desc = "Project diagnostics" })
-    end
+    vim.keymap.set("n", "<leader>pe", function()
+      -- Get all diagnostics from the workspace
+      vim.diagnostic.setqflist({
+        severity = nil,  -- Get all severities
+        workspace = true -- Get diagnostics from all files in workspace
+      })
+    end, { desc = "Project diagnostics" })
+  end
 })
 
 -- add relative line numbers to built-in-file explore (netrw)
--- lua/leblanq/plugins/lsp.lua
+++ lua/leblanq/plugins/lsp.lua
@@ -1,220 +1,220 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"stevearc/conform.nvim",
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"j-hui/fidget.nvim",
-	},
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "stevearc/conform.nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "j-hui/fidget.nvim",
+  },
 
-	config = function()
-		require("conform").setup({
-			log_level = vim.log.levels.DEBUG,
-			formatters_by_ft = {
-				javascript = { "prettierd", "prettier", lsp_format = "fallback" },
-				typescript = { "prettierd", "prettier", lsp_format = "fallback" },
-				lua = { "stylua", lsp_format = "falllback" },
-				rust = { "rustfmt", lsp_format = "fallback" },
-				go = { "gofmt", lsp_format = "fallback" },
-				elixir = function(bufnr)
-					if require("conform").get_formatter_info("mix", bufnr).available then
-						return { "mix" }
-					else
-						return { lsp_format = "fallback" }
-					end
-				end,
-				python = function(bufnr)
-					local conform = require("conform")
-					if conform.get_formatter_info("ruff_format", bufnr).available then
-						return { "ruff_format" }
-					elseif
-						conform.get_formatter_info("isort", bufnr).available
-						or conform.get_formatter_info("black", bufnr).available
-					then
-						return { "isort", "black" }
-					else
-						return { lsp_format = "fallback" }
-					end
-				end,
-			},
-			format_on_save = {
-				timeout_ms = 2000,
-				lsp_format = "fallback",
-				debug = true,
-			},
-		})
-		local cmp = require("cmp")
-		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities()
-		)
+  config = function()
+    require("conform").setup({
+      log_level = vim.log.levels.DEBUG,
+      formatters_by_ft = {
+        javascript = { "prettierd", "prettier", lsp_format = "fallback" },
+        typescript = { "prettierd", "prettier", lsp_format = "fallback" },
+        lua = { "stylua", lsp_format = "falllback" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        go = { "gofmt", lsp_format = "fallback" },
+        elixir = function(bufnr)
+          if require("conform").get_formatter_info("mix", bufnr).available then
+            return { "mix" }
+          else
+            return { lsp_format = "fallback" }
+          end
+        end,
+        python = function(bufnr)
+          local conform = require("conform")
+          if conform.get_formatter_info("ruff_format", bufnr).available then
+            return { "ruff_format" }
+          elseif
+              conform.get_formatter_info("isort", bufnr).available
+              or conform.get_formatter_info("black", bufnr).available
+          then
+            return { "isort", "black" }
+          else
+            return { lsp_format = "fallback" }
+          end
+        end,
+      },
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_format = "fallback",
+        debug = true,
+      },
+    })
+    local cmp = require("cmp")
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities()
+    )
 
-		require("fidget").setup({})
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"rust_analyzer",
-				"gopls",
-				"elixirls",
-				"tailwindcss",
-				"eslint",
-				"ts_ls",
-				"pyright",
-				-- "ruff",
-			},
-			handlers = {
-				function(server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
+    require("fidget").setup({})
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "gopls",
+        "elixirls",
+        "tailwindcss",
+        "eslint",
+        "ts_ls",
+        "pyright",
+        -- "ruff",
+      },
+      handlers = {
+        function(server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
 
-				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								runtime = { version = "Lua 5.1" },
-								diagnostics = {
-									globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-								},
-							},
-						},
-					})
-				end,
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                runtime = { version = "Lua 5.1" },
+                diagnostics = {
+                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                },
+              },
+            },
+          })
+        end,
 
-				["eslint"] = function()
-					local lspconfig = require("lspconfig")
+        ["eslint"] = function()
+          local lspconfig = require("lspconfig")
 
-					lspconfig.eslint.setup({
-						capabilities = capabilities,
-						-- root_dir = require("lspconfig.util").root_pattern(".eslintrc", ".eslintrc.json", ".eslintrc.js"),
-						settings = {
-							workingDirectory = { mode = "location" },
-							format = true,
-						},
-						root_dir = lspconfig.util.find_git_ancestor,
-					})
-				end,
+          lspconfig.eslint.setup({
+            capabilities = capabilities,
+            -- root_dir = require("lspconfig.util").root_pattern(".eslintrc", ".eslintrc.json", ".eslintrc.js"),
+            settings = {
+              workingDirectory = { mode = "location" },
+              format = true,
+            },
+            root_dir = lspconfig.util.find_git_ancestor,
+          })
+        end,
 
-				["elixirls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.elixirls.setup({
-						capabilities = capabilities,
-						cmd = { "elixir-ls" },
-						-- root_dir = lspconfig.util.root_pattern("mix.exs", ".git"),
-						settings = {
-							workingDirectory = { mode = "location" },
-							format = true,
-						},
-						root_dir = lspconfig.util.find_git_ancestor,
-					})
-				end,
+        ["elixirls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.elixirls.setup({
+            capabilities = capabilities,
+            cmd = { "elixir-ls" },
+            -- root_dir = lspconfig.util.root_pattern("mix.exs", ".git"),
+            settings = {
+              workingDirectory = { mode = "location" },
+              format = true,
+            },
+            root_dir = lspconfig.util.find_git_ancestor,
+          })
+        end,
 
-				["tailwindcss"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.tailwindcss.setup({
-						capabilities = capabilities,
-						cmd = { "tailwindcss-language-server", "--stdio" },
-						filetypes = {
-							"html",
-							"css",
-							"scss",
-							"javascript",
-							"javascriptreact",
-							"typescript",
-							"typescriptreact",
-						},
-						root_dir = lspconfig.util.root_pattern(
-							"tailwind.config.js",
-							"tailwind.config.cjs",
-							"tailwind.config.ts"
-						),
-					})
-				end,
+        ["tailwindcss"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.tailwindcss.setup({
+            capabilities = capabilities,
+            cmd = { "tailwindcss-language-server", "--stdio" },
+            filetypes = {
+              "html",
+              "css",
+              "scss",
+              "javascript",
+              "javascriptreact",
+              "typescript",
+              "typescriptreact",
+            },
+            root_dir = lspconfig.util.root_pattern(
+              "tailwind.config.js",
+              "tailwind.config.cjs",
+              "tailwind.config.ts"
+            ),
+          })
+        end,
 
-				["ts_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.ts_ls.setup({
-						capabilities = capabilities,
-						root_dir = lspconfig.util.root_pattern(
-							"package.json",
-							"tsconfig.json",
-							"jsconfig.json",
-							".git"
-						),
-					})
-				end,
+        ["ts_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.ts_ls.setup({
+            capabilities = capabilities,
+            root_dir = lspconfig.util.root_pattern(
+              "package.json",
+              "tsconfig.json",
+              "jsconfig.json",
+              ".git"
+            ),
+          })
+        end,
 
-				["pyright"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.pyright.setup({
-						capabilities = capabilities,
-						-- Increase Node.js memory limit (default is 4096 MB)
-						cmd = {
-							"node",
-							"--max-old-space-size=4096", -- Increase to 4GB (adjust as needed)
-							vim.fn.expand("~/.local/share/nvim/mason/bin/pyright-langserver"),
-							"--stdio",
-						},
-						settings = {
+        ["pyright"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.pyright.setup({
+            capabilities = capabilities,
+            -- Increase Node.js memory limit (default is 4096 MB)
+            cmd = {
+              "node",
+              "--max-old-space-size=4096", -- Increase to 4GB (adjust as needed)
+              vim.fn.expand("~/.local/share/nvim/mason/bin/pyright-langserver"),
+              "--stdio",
+            },
+            settings = {
 
-							workingDirectory = { mode = "location" },
-							format = true,
-							python = {
-								pythonPath = vim.fn.exepath("python"), -- Use your Python interpreter
-							},
-						},
-						root_dir = lspconfig.util.find_git_ancestor,
-					})
-				end,
-			},
-		})
+              workingDirectory = { mode = "location" },
+              format = true,
+              python = {
+                pythonPath = vim.fn.exepath("python"), -- Use your Python interpreter
+              },
+            },
+            root_dir = lspconfig.util.find_git_ancestor,
+          })
+        end,
+      },
+    })
 
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-				["<Tab>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- For luasnip users.
-			}, {
-				{ name = "buffer" },
-			}),
-		})
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+      }, {
+        { name = "buffer" },
+      }),
+    })
 
-		vim.diagnostic.config({
-			-- update_in_insert = true,
-			float = {
-				focusable = false,
-				style = "minimal",
-				border = "rounded",
-				source = "always",
-				header = "",
-				prefix = "",
-			},
-		})
-	end,
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+  end,
 }
